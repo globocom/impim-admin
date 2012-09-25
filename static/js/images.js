627f83c6fc3536e11702d151614352524b1daa33
Guilherme Cirne <gcirne@gmail.com>
2012-09-25 10:49:00 -0300

    missing css files.

--- static/js/images.js
+++ static/js/images.js
@@ -1,6 +1,11 @@
 (function($) {
     $(function() {
-        $('.popin').fotoPopin();
+        $('.popin').fotoPopin({
+            urls: {
+                search: 'http://localhost:8080/alpha/search',
+                unsafeUrl: 'http://localhost:8888/unsafe'
+            }
+        });
         $('.images-popin').on('click', function() {
             $('.popin').fotoPopin('open');
         });
