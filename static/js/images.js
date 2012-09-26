2e54617b1f05f649b4802333b77f42b2155862b3
Guilherme Cirne <gcirne@gmail.com>
2012-09-26 14:11:19 -0300

    refactor js to make the default case simple. see #1.

--- static/js/images.js
+++ static/js/images.js
@@ -1,6 +1,10 @@
-(function($) {
-    $(function() {
-        $('.popin').fotoPopin({
+(function(window, $) {
+    window.images = window.images || {};
+    
+    window.images.init = function() {
+        _appendImagesPopin();
+        
+        $('.images-popin').fotoPopin({
             urls: {
                 search: 'http://localhost:8080/alpha/search',
                 generateUrl: 'http://localhost:8080/thumbor_urls',
@@ -8,12 +12,22 @@
             }
         });
         
-        $('.popin').unbind('fotoPopinImageApplied').bind('fotoPopinImageApplied', function(event, data) {
-            $('.img-container').html('<img src="' + data.croppedUrl + '" />');
+        $('.images-popin').unbind('fotoPopinImageApplied').bind('fotoPopinImageApplied', function(event, data) {
+            $('.images-popin-target').attr('src', data.croppedUrl);
         });
         
-        $('.images-popin').on('click', function() {
-            $('.popin').fotoPopin('open');
+        $('.images-popin-click-open').bind('click', function() {
+            $('.images-popin').fotoPopin('open');
         });
+    };
+    
+    var _appendImagesPopin = function() {
+        if ($('.images-popin').length === 0) {
+            $('body').append('<div class="images-popin"></div>');
+        }
+    };
+    
+    $(function() {
+        images.init();
     });
-})(jQuery);
+})(window, jQuery);
---
+++ tests/js/popin/specs/images.spec.js
@@ -0,0 +1,51 @@
+describe('images init', function() {
+    
+    describe("when clicking on images-popin-click-open and .images-popin div doesn't exist yet", function() {
+        beforeEach(function() {
+            setFixtures('<input type="button" class="images-popin-click-open" />');
+            images.init();
+            $('.images-popin-click-open').click();
+        });
+        
+        afterEach(function() {
+            $('.images-popin').fotoPopin('close');
+        });
+        
+        it('should open images popin', function() {
+            expect($('.images-fotoPopin-container')).toBeVisible();
+        });
+    });
+    
+    describe('when clicking on images-popin-click-open and .images-popin div already exists', function() {
+        beforeEach(function() {
+            setFixtures('<input type="button" class="images-popin-click-open" /><div class="images-popin></div>"');
+            images.init();
+            $('.images-popin-click-open').click();
+        });
+        
+        afterEach(function() {
+            $('.images-popin').fotoPopin('close');
+        });
+        
+        it('should open images popin', function() {
+            expect($('.images-fotoPopin-container')).toBeVisible();
+        });
+        
+        it('should have only one .images-popin div', function() {
+            expect($('.images-popin').length).toBe(1);
+        });
+    });
+    
+    describe('when fotoPopinImageApplied is fired and images-popin-target is an img', function() {
+        beforeEach(function() {
+            setFixtures('<img class="images-popin-target" />');
+            images.init();
+            $('.images-popin').trigger('fotoPopinImageApplied', {croppedUrl: 'http://example.com/croppedUrl'});
+        });
+        
+        it("should apply cropped url to img's src attribute", function() {
+            expect($('.images-popin-target')).toHaveAttr('src', 'http://example.com/croppedUrl');
+        });
+    });
+    
+});
