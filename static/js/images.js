38e0097742e548ba53feba43c60470a3e2963e10
Guilherme Cirne <gcirne@gmail.com>
2012-09-26 14:18:16 -0300

    splits images.js into images.init.js & images.js.

--- static/js/images.js
+++ static/js/images.js
@@ -1,33 +1,3 @@
-(function(window, $) {
-    window.images = window.images || {};
-    
-    window.images.init = function() {
-        _appendImagesPopin();
-        
-        $('.images-popin').fotoPopin({
-            urls: {
-                search: 'http://localhost:8080/alpha/search',
-                generateUrl: 'http://localhost:8080/thumbor_urls',
-                unsafeUrl: 'http://localhost:8888/unsafe'
-            }
-        });
-        
-        $('.images-popin').unbind('fotoPopinImageApplied').bind('fotoPopinImageApplied', function(event, data) {
-            $('.images-popin-target').attr('src', data.croppedUrl);
-        });
-        
-        $('.images-popin-click-open').bind('click', function() {
-            $('.images-popin').fotoPopin('open');
-        });
-    };
-    
-    var _appendImagesPopin = function() {
-        if ($('.images-popin').length === 0) {
-            $('body').append('<div class="images-popin"></div>');
-        }
-    };
-    
-    $(function() {
-        images.init();
-    });
-})(window, jQuery);
+$(function() {
+    images.init();
+});
